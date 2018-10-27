9513de5af57fedff87905980108c9a0c73cde2a9
zeneros <benz_8500_@hotmail.com>
2018-10-28 00:06:56 +0700

    - Add new function login

--- kaimookbnk48fc-admin/src/app/auth/login/login.component.ts
+++ kaimookbnk48fc-admin/src/app/auth/login/login.component.ts
@@ -6,4 +6,18 @@ import { NbLoginComponent } from '@nebular/auth';
   templateUrl: './login.component.html',
 })
 export class LoginComponent extends NbLoginComponent {
+
+  login2() {
+    if (this.user.username == "admin" && this.user.password == "1234") {
+      // console.log("success login", this);  
+      this.showMessages.success = true;
+      this.showMessages.error = false;
+      this.messages.push("success login");
+    } else {
+      // console.log("fail login", this);
+      this.showMessages.success = false;
+      this.showMessages.error = true;
+      this.errors.push("fail login");
+    }
+  }
 }
